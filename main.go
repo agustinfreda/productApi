df9d3ed12cefb737da13d007ff0d533a8a3ae624
Agustin Freda <agustinfreda2004@gmail.com>
2025-02-27 23:55:38 -0300

    storage of products

--- main.go
+++ main.go
@@ -15,6 +15,23 @@ type Product struct {
 	Stock int    `json:Stock`
 }
 
+type allProducts []Product
+
+var products = allProducts{
+	{
+		ID:    1,
+		Name:  "Product One",
+		Price: 10,
+		Stock: 250,
+	},
+	{
+		ID:    2,
+		Name:  "Product Two",
+		Price: 100,
+		Stock: 20,
+	},
+}
+
 func indexRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to my API")
 }
