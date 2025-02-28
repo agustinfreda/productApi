647a468e873cc3f5a6141c8a2c0fe4e56473d101
Agustin Freda <agustinfreda2004@gmail.com>
2025-02-27 23:50:37 -0300

    http initial configuration

--- main.go
+++ main.go
@@ -1,2 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my API")
+}
+
+func main() {
+	// Enrutador
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Rutas
+	router.HandleFunc("/", indexRoute)
+
+	// Server HTTP
+	log.Fatal(http.ListenAndServe(":4567", router))
+}
